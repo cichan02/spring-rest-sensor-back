238b4bf01c7727dfdbdf696476ece838e5c08cc3
cichan <6684177pt@gmail.com>
2022-11-26 15:15:42 +0300

    init

---
+++ src/main/java/by/piskunou/springcourse/SpringRestSensorBackApplication.java
@@ -0,0 +1,13 @@
+package by.piskunou.springcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSensorBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestSensorBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/piskunou/springcourse/SpringRestSensorBackApplicationTests.java
@@ -0,0 +1,13 @@
+package by.piskunou.springcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestSensorBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
